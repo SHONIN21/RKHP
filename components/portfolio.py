337b094d803e4b15229f750058e3cdc4f6d92e31
SHONIN21 <kensei0421t@gmail.com>
2024-05-15 15:35:49 +0900

    width

--- components/portfolio.py
+++ components/portfolio.py
@@ -8,12 +8,12 @@ def portfolio(language):
     tabs = st.tabs([texts["portfolio1"][0],texts["portfolio1"][1],texts["portfolio1"][2]])
     with tabs[0]:
         st.write(texts["portfolio2"][0])
-        st.image("./img/po0.png",width=800)
+        st.image("./img/po0.png",use_column_width=True)
     with tabs[1]:
         st.write(texts["portfolio2"][1])
-        st.image("./img/po1.png",width=800)
+        st.image("./img/po1.png",use_column_width=True)
     with tabs[2]:
         st.write(texts["portfolio2"][2])
-        st.image("./img/po2.png",width=800)
+        st.image("./img/po2.png",use_column_width=True)
     
     st.markdown('---')
